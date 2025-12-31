0665e5ef0f856df065e3b54270fbabd429f234df
Kota Hatanaka <kenkotan7722@gmail.com>
2025-12-31 11:00:27 +0900

    演算子バグ修正

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -280,9 +280,9 @@ public class GameManager : MonoBehaviour
             return true;
         }
         // 5. cardToPlay.numberValue == currentTrendValue（数字が同じ）かつffectがReject, Audit, Suspendのいずれかであればtrue
-        if(cardToPlay.effect == CardEffect.Audit ||
+        if((cardToPlay.effect == CardEffect.Audit ||
             cardToPlay.effect == CardEffect.Reject ||
-            cardToPlay.effect == CardEffect.Suspend &&
+            cardToPlay.effect == CardEffect.Suspend) &&
             (cardToPlay.numberValue == currentTrendValue))
         {
             return true;
