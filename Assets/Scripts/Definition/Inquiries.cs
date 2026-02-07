11600d4aac5db91616357d9e6e7682dab4711517
Kota Hatanaka <kenkotan7722@gmail.com>
2026-02-07 17:11:57 +0900

    パッと思いつかんのよな

--- Assets/Scripts/Definition/Inquiries.cs
+++ Assets/Scripts/Definition/Inquiries.cs
@@ -1,13 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "Inquiries", menuName = "Scriptable Objects/Inquiries")]
 public class Inquiries : ScriptableObject
 {
     [Header("基本情報")]
+    [TextArea(3, 10)]
     public string title;
     public bool hasPhoto;
-    public List<Button> answers;
+    public GameObject answer;
     public List<Sprite> photos;
 }
